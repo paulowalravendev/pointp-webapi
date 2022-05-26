b06c52bea3442a4853ba79aac68dc2e34213bd9b
Paulo Walraven <pmwc03@gmail.com>
2022-05-26 20:09:13 -0300

    clean project and move swagger setup

---
+++ PointPWebApi/Infrastructure/StartupSetups/SwaggerSetup.cs
@@ -0,0 +1,40 @@
+namespace PointPWebApi.Infrastructure.StartupSetups;
+
+public static class SwaggerSetup
+{
+    public static IServiceCollection AddSwaggerSetup(this IServiceCollection services)
+        => services.AddSwaggerGen(setupAction =>
+        {
+            setupAction.SwaggerDoc(
+                "PointPWebApiSpecification",
+                new OpenApiInfo
+                {
+                    Title = "PointPWebApi",
+                    Version = "1",
+                    Description = "Description about PointPWebApiSpecification",
+                    TermsOfService = new Uri("https://www.terms-of-services.com"),
+                    License = new OpenApiLicense
+                    {
+                        Name = "License name",
+                        Url = new Uri("https://www.license.com")
+                    },
+                    Contact = new OpenApiContact
+                    {
+                        Email = "owner@mail.com",
+                        Name = "Owner name",
+                        Url = new Uri("https://www.contact.com")
+                    }
+                });
+            setupAction.EnableAnnotations();
+        });
+    
+    public static IApplicationBuilder UseSwaggerSetup(this IApplicationBuilder app)
+        => app.UseSwagger()
+            .UseSwaggerUI(setupAction =>
+            {
+                setupAction.SwaggerEndpoint(
+                    "/swagger/PointPWebApiSpecification/swagger.json",
+                    "PointPWeb API");
+                setupAction.RoutePrefix = "";
+            });
+}
--- PointPWebApi/Program.cs
+++ PointPWebApi/Program.cs
@@ -1,25 +1,12 @@
 var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+var services = builder.Services;
+services.AddControllers();
+services.AddEndpointsApiExplorer();
+services.AddSwaggerSetup();
 
 var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-
+app.UseSwaggerSetup();
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
 app.MapControllers();
-
 app.Run();
